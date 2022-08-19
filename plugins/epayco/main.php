5e78d0bf8b2af2dc05e5c53c903f6d3368ab5b31
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2022-08-19 15:44:18 -0500

    feat:codigo oficial del plugin

--- plugins/epayco/main.php
+++ plugins/epayco/main.php
@@ -1,2 +1,14 @@
 <?php
+$txnId = sprintf("%08x", crc32(microtime().'|'.$pluginData->pkey));
 ?>
+<input type="hidden" name="surl" value="<?php echo getBaseUrl().'store-return/Epayco'; ?>" />
+<input type="hidden" name="totalAmount" value="<?php echo $pluginData->amount; ?>" />
+<input type="hidden" name="subTotalPrice" value="<?php echo $pluginData->amount; ?>" />
+<input type="hidden" name="taxPrice" value="0" />
+<input type="hidden" name="txnid" value="<?php echo $txnId; ?>" />
+<input type="hidden" name="countryCode" value="" />
+<input type="hidden" name="invoiceNumber" value="" />
+<input type="hidden" name="extra1" value="<?php echo $txnId; ?>" />
+<input type="hidden" name="customer_name" value="" />
+<input type="hidden" name="customer_email" value="" />
+<input type="hidden" name="address1" value="" />
